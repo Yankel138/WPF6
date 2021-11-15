7d29cf231b24cf6c885086e8a638cf536c5e349f
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-11-15 19:05:06 +0300

    Добавьте файлы проекта.

---
+++ WPF6/Class1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF6
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        private int windSpeed;
+        private string windDirection;
+        private string weather;
+        public enum WeatherType
+        {
+            Sun,
+            Cloud,
+            Rain,
+            Snow
+        }
+
+        public WeatherControl(int temperature, string windDirection, int windSpeed, WeatherType wheater)
+        {
+            this.Temperature = temperature;
+            this.WindDirection = windDirection;
+            this.WindSpeed = windSpeed;
+            this.Weather = weather;
+        }
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public string Weather
+        {
+            get
+            {
+                return weather;
+            }
+            set
+            {
+                weather = value;
+            }
+        }
+
+
+        public string WindDirection
+        {
+            get
+            {
+                return windDirection;
+            }
+            set
+            {
+                windDirection = value;
+            }
+        }
+        public int WindSpeed
+        {
+            get
+            {
+                return windSpeed;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    windSpeed = value;
+                }
+                else
+                {
+                    windSpeed = 0;
+                }
+            }
+        }
+
+
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)));
+
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
